51255500edb7748721cf6111fb84ff9bb26ae1c6
Hameed Al Naser <alnaser@sihatkom.com>
2020-06-07 15:36:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-function = 
+function =
 dogimage = document.getElementById('dog-image-container')
 img = document.createElement('img')
